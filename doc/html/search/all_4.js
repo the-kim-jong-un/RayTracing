5fcdfa55152e340bed98e46ca81106f5e58bf2ec
alban <alban.saint-sorny@etu.univ-lyon1.fr>
2021-01-12 18:16:48 +0100

    doxy

--- doc/html/search/all_4.js
+++ doc/html/search/all_4.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['mainwindow_11',['MainWindow',['../classMainWindow.html',1,'MainWindow'],['../classMainWindow.html#a996c5a2b6f77944776856f08ec30858d',1,'MainWindow::MainWindow()']]],
-  ['material_12',['Material',['../classMaterial.html',1,'Material'],['../classMaterial.html#ac5eb84a898daa6a246dea42070131612',1,'Material::Material()']]],
+  ['material_12',['Material',['../classMaterial.html',1,'Material'],['../classMaterial.html#aec993d57c8d6824b63c975eda2986b3a',1,'Material::Material()']]],
   ['matrix_13',['Matrix',['../classMatrix.html',1,'']]],
   ['matrix4x4_14',['Matrix4x4',['../classMatrix4x4.html',1,'']]]
 ];
--- doc/html/search/functions_4.js
+++ doc/html/search/functions_4.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['mainwindow_67',['MainWindow',['../classMainWindow.html#a996c5a2b6f77944776856f08ec30858d',1,'MainWindow']]],
-  ['material_68',['Material',['../classMaterial.html#ac5eb84a898daa6a246dea42070131612',1,'Material']]]
+  ['material_68',['Material',['../classMaterial.html#aec993d57c8d6824b63c975eda2986b3a',1,'Material']]]
 ];
